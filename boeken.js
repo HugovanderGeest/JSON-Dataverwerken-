c15d2ddcb1efd18a7ccac9dcf6bcc35dbd9999be
Hugo van der Geest <Hugo.vdg@outlook.com>
2020-10-27 13:27:21 +0100

    2.1

--- boeken.js
+++ boeken.js
@@ -1,17 +1,15 @@
-const output = document.getElementById(`boeken`);
+const uitvoer = document.getElementById('boeken');
 const xhr = new XMLHttpRequest();
 
-const taalKeuze = document.querySelectorAll(`.besturing__cb-taal`);
-
 xhr.onreadystatechange = () => {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-        let xhrResult = JSON.parse(xhr.responseText);
+    if(xhr.readyState == 4 && xhr.status ==200) {
+        let resultaat = JSON.parse(xhr.responseText);
+        console.log(resultaat);
 
-        boeken.filteren(xhrResult);
-        boeken.uitvoeren();
     }
 }
 
-xhr.open(`GET`, `boeken.json`, true);
-
+xhr.open('GET', 'boeken.json', true);
 xhr.send();
+
+console.log( 'dit is de laaste regel')
