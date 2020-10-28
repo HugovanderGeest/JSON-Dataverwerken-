5e874f16aad1d92d90b81c158985ed1cd595a3cd
Hugo van der Geest <Hugo.vdg@outlook.com>
2020-10-28 16:46:23 +0100

    6

--- boeken.js
+++ boeken.js
@@ -25,12 +25,12 @@ const boeken = {
             }
             completeTitel += boek.titel;
 
-            // auteurs
+            // auteurs html
 
             let auteurs ="";
             boek.auteurs.forEach(schrijver => {
                 let tussenvoegsel = schrijver.tussenvoegsel ? schrijver.tussenvoegsel+" " : " ";
-                auteurs += schrijver.voornaam + " " + tussenvoegsel + " " + schrijver.achternaam  + " | ";
+                auteurs += schrijver.voornaam + " " + tussenvoegsel + " " + schrijver.achternaam  + ", " ;
             })
 
 
@@ -40,12 +40,65 @@ const boeken = {
             html += `<h3>${completeTitel}</h3>`;
             html += `<p class="boek__auteurs">Auteur(s): ${auteurs}</p>`
             html += `<span class="boek_ean">EAN: ${boek.ean} | </span>`
-            html += `<span class="boek_uitvage">Uitgaven datum: ${boek.uitgave} | </span>`
+            html += `<span class="boek_uitvage">Uitgaven datum: ${this.datumOmzetten(boek.uitgave)} | </span>`
             html += `<span class="boek_taal">Taal: ${boek.taal} | </span>`
             html += `<span class="boek_paginas">Aantal blatzijden: ${boek.paginas} </span>`
-            html += `<div class="boek_prijs">&euro; ${boek.prijs} </div>`
+            html += `<div class="boek_prijs"> ${boek.prijs.toLocaleString('nl-NL', {currency: 'EUR', style: 'currency'})} </div>`
             html += `</section>`;
         });
         uitvoer.innerHTML = html
+    },
+
+    datumOmzetten(datumString) {
+        let datum = new Date(datumString);
+        let jaar = datum.getFullYear();
+        let maand = this.geefMaandnaam(datum.getMonth());
+
+        return `${maand} ${jaar}`;
+    },
+
+    geefMaandnaam(m) {
+        let maand = "";
+        switch (m) {
+            case 0:
+                maand = `januari`;
+                break;
+            case 1:
+                maand = `februari`;
+                break;
+            case 2:
+                maand = `maart`;
+                break;
+            case 3:
+                maand = `april`;
+                break;
+            case 4:
+                maand = `mei`;
+                break;
+            case 5:
+                maand = `juni`;
+                break;
+            case 6:
+                maand = `juli`;
+                break;
+            case 7:
+                maand = `augustus`;
+                break;
+            case 8:
+                maand = `september`;
+                break;
+            case 9:
+                maand = `oktober`;
+                break;
+            case 10:
+                maand = `november`;
+                break;
+            case 11:
+                maand = `december`;
+                break;
+            default:
+                maand = m;
+        }
+        return maand;
     }
 }
