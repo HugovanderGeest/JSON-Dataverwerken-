34d05d9990e59a10583cefd868b3dda22e7ce734
Hugo van der Geest <Hugo.vdg@outlook.com>
2020-12-04 13:09:36 +0100

    vid 20

--- boeken.js
+++ boeken.js
@@ -73,7 +73,8 @@ sorteren() {
             html += `<span class="boek_uitvage">Uitgaven datum: ${this.datumOmzetten(boek.uitgave)} | </span>`
             html += `<span class="boek_taal">Taal: ${boek.taal} | </span>`
             html += `<span class="boek_paginas">Aantal blatzijden: ${boek.paginas} </span>`
-            html += `<div class="boek_prijs"> ${boek.prijs.toLocaleString('nl-NL', {currency: 'EUR', style: 'currency'})} </div>`
+            html += `<a class="none" href="#"><div class="boek_prijs"> ${boek.prijs.toLocaleString('nl-NL', {currency: 'EUR', style: 'currency'})} 
+            <a href="#" class="boek__bestel-knop">Bestellen</a></div></a>`;
             html += `</section>`;
         });
         uitvoer.innerHTML = html
